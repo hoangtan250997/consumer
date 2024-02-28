ca33b512d207596fb8c00eecbe6ecc130e5f9781
nhtan <hoangtan250997@gmail.com>
2024-02-28 11:16:02 +0700

    [CHANGE] name

--- src/main/java/com/example/comsumer/Consumer.java
+++ src/main/java/com/example/comsumer/Consumer.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class Consumer {
-    private static final String orderTopic = "${topic.name.drink}";
+    private static final String orderTopic = "t.drink.order";
 
     private final ObjectMapper objectMapper;
     private final OrderService foodOrderService;
